714880e348271cafe7fa520596623189db5f943f
kodamayuto <yuto2001gyokugan@gmail.com>
2021-05-16 22:26:31 +0900

    Sign in UI

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,102 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <div class="input-container">
+      <div class="mail">
+        <!-- <span class="material-icons">account_circle</span> -->
+        <vs-icon icon="account_circle" size="50px"></vs-icon>
+        <vs-input
+        :success="true"
+        placeholder="Email Success"
+        v-model="value1" 
+        class="input"
+        size="large" 
+        />
+      </div>      
+
+      <div class="password">
+        <!-- <span class="material-icons" size="75px">lock</span> -->
+        <vs-icon icon="lock" size="50px"></vs-icon>
+        <vs-input
+        :danger="true"
+        placeholder="Password Error"
+        v-model="value2"
+        class="input"
+        size="large"
+        type="password"/>
+        <a href=""><small>Forget Password ?</small></a>
+      </div>
+
+      <div class="btn">
+        <vs-button color="success" type="relief" size="large"><strong>Sign up</strong></vs-button>
+
+        <vs-button color="success" type="relief" size="large">Sign in</vs-button>
+      </div>
+
+
+    </div>
+
   </div>
+
+
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data(){
+    return {
+      value1:"",
+      value2:"",
+    }
   }
 }
+
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+
+<style>
+@import url(https://fonts.googleapis.com/icon?family=Material+Icons);
+
+.container {
+  background-color: #DDD;
+  
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.container .input-container {
+  margin-left: 500px;
+  margin-right: 500px;
+  background-color: #CCC;
+  align-items: center;
+  border-radius: 10px;
+  margin: 50px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.container .input-container .input {
+  /* background-color: #BBB; */
+  width: 500px;
 }
-a {
-  color: #42b983;
+
+.mail {
+  margin:30px;
+  /* background-color: #AAA; */
 }
+
+.password {
+  margin: 30px;
+  /* background-color: #AAA; */
+}
+
+.btn {
+  margin: 10px;
+}
+
+.vs-button {
+  width: 200px;
+  margin-left: 10px;
+  margin-right: 10px;
+}
+
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuesax from 'vuesax'
 
-Vue.config.productionTip = false
+import 'vuesax/dist/vuesax.css' //Vuesax styles
+Vue.use(Vuesax, {
+  // options here
+})
 
 new Vue({
+  Vuesax,
   render: h => h(App),
 }).$mount('#app')
